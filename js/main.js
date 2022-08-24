516622631c2c98316c7e1c8df49a8507a8e27607
AgustinTrigo(re) <a_trigo295@hotmail.com>
2022-08-24 15:24:45 -0300

    Jugabilidad basica

--- js/main.js
+++ js/main.js
@@ -1,61 +1,18 @@
 let tablero = document.getElementById("table");
 
-
-/*
-for(let i = 1; i <= 9; i++){
-    
-    tablero.innerHTML += `<div id="1" class="casilla" onClick="marcador()"></div>`
-}
-*/
-
 const casillas = [
-    {
-        id:1,
-        container: ""
-    },
-    {
-        id:2,
-        container: ""
-    },
-    {
-        id:3,
-        container: ""
-    },
-    {
-        id:4,
-        container: ""
-    },
-    {
-        id:5,
-        container: ""
-    },
-    {
-        id:6,
-        container: ""
-    },
-    {
-        id:7,
-        container: ""
-    },
-    {
-        id:8,
-        container: ""
-    },
-    {
-        id:9,
-        container: ""
-    },
+    {id:1, container: ""},
+    {id:2, container: ""},
+    {id:3, container: ""},
+    {id:4, container: ""},
+    {id:5, container: ""},
+    {id:6, container: ""},
+    {id:7, container: ""},
+    {id:8, container: ""},
+    {id:9, container: ""}
 ]
 
-const loadTablero = () => { localStorage.setItem("tablero", JSON.stringify(casillas))};
-
-
-const getTablero = () => {
-    return tableroLocal = JSON.parse(localStorage.getItem("tablero"));
-}
-
-loadTablero();
-
+// Iconos
 let xmark =  `<i class="fa-solid fa-xmark"></i>`;
 let circle = `<i class="fa-regular fa-circle"></i>`;
 
@@ -68,27 +25,107 @@ let contador = 0;
 const jugadorUno = [];
 const jugadorDos = [];
 
+// Objetos para validar si se completo la linea.
+
+const validacionesCirculo = {
+    A1: false,
+    A2: false,
+    A3: false,
+    B1: false,
+    B2: false,
+    B3: false,
+    C1: false,
+    C2: false,
+    C3: false,
+}
+
+const validacionesXmark = {
+    A1: false,
+    A2: false,
+    A3: false,
+    B1: false,
+    B2: false,
+    B3: false,
+    C1: false,
+    C2: false,
+    C3: false,
+}
+
+// Funcion para marcar el tablero por turnos.
+
 function marcador(index){
     let casilla = index + 1;
     let slot = document.getElementById(`${casilla}`);
     if((jugadorUno.length < 5) && (jugadorUno.length < 5)){ 
-        if((contador % 2 == 0) && (!jugadorDos.includes(casilla, -1)) && (!jugadorUno.includes(casilla, -1))){
-            console.log((!jugadorDos.includes(casilla, -1)))
-            console.log((!jugadorUno.includes(casilla, -1)))
+        if((contador % 2 == 0) && (!jugadorDos.includes(casilla, -10)) && (!jugadorUno.includes(casilla, -10))){
             slot.innerHTML = circle;
             contador++;
-            jugadorUno.push(index + 1);
-            console.log(`Jugador A ${jugadorUno}`);
-        }else if((!jugadorUno.includes(casilla, -1)) && (!jugadorDos.includes(casilla, -1))){
-            console.log((!jugadorDos.includes(casilla, -1)))
-            console.log((!jugadorUno.includes(casilla, -1)))
+            jugadorUno.push(casilla);
+            validar(jugadorUno, validacionesCirculo);
+        }else if((!jugadorUno.includes(casilla, -10)) && (!jugadorDos.includes(casilla, -10))){
             slot.innerHTML = xmark;
             contador++;
-            jugadorDos.push(index + 1);
-            console.log(`Jugador B ${jugadorDos}`);
+            jugadorDos.push(casilla);
+            validar(jugadorDos, validacionesXmark);
         }
     }
-    console.log(contador);
+}
+
+// Funcion para validar ganador
+
+function validar(objeto1, objeto2){
+    if(objeto1.includes(1,-10)){
+        objeto2.A1 = true;
+    }
+    if(objeto1.includes(2,-10)){
+        objeto2.A2 = true;
+    }
+    if(objeto1.includes(3,-10)){
+        objeto2.A3 = true;
+    }
+    if(objeto1.includes(4,-10)){
+        objeto2.B1 = true;
+    }
+    if(objeto1.includes(5,-10)){
+        objeto2.B2 = true;
+    }
+    if(objeto1.includes(6,-10)){
+        objeto2.B3 = true;
+    }
+    if(objeto1.includes(7,-10)){
+        objeto2.C1 = true;
+    }
+    if(objeto1.includes(8,-10)){
+        objeto2.C2 = true;
+    }
+    if(objeto1.includes(9,-10)){
+        objeto2.C3 = true;
+    }
+    if((objeto2.A1 == true) && (objeto2.A2 == true) && (objeto2.A3 == true)){
+        document.getElementById("hud").innerHTML = "<h2>GANASTE</h2>";
+    }
+    if((objeto2.B1 == true) && (objeto2.B2 == true) && (objeto2.B3 == true)){
+        document.getElementById("hud").innerHTML = "<h2>GANASTE</h2>";
+    }
+    if((objeto2.C1 == true) && (objeto2.C2 == true) && (objeto2.C3 == true)){
+        document.getElementById("hud").innerHTML = "<h2>GANASTE</h2>";
+    }
+    if((objeto2.A1 == true) && (objeto2.B1 == true) && (objeto2.C1 == true)){
+        document.getElementById("hud").innerHTML = "<h2>GANASTE</h2>";
+    }
+    if((objeto2.A2 == true) && (objeto2.B2 == true) && (objeto2.C2 == true)){
+        document.getElementById("hud").innerHTML = "<h2>GANASTE</h2>";
+    }
+    if((objeto2.A3 == true) && (objeto2.B3 == true) && (objeto2.C3 == true)){
+        document.getElementById("hud").innerHTML = "<h2>GANASTE</h2>";
+    }
+    if((objeto2.A1 == true) && (objeto2.B2 == true) && (objeto2.C3 == true)){
+        document.getElementById("hud").innerHTML = "<h2>GANASTE</h2>";
+    }
+    if((objeto2.C1 == true) && (objeto2.B2 == true) && (objeto2.A3 == true)){
+        document.getElementById("hud").innerHTML = "<h2>GANASTE</h2>";
+    }
+
 }
 
 
